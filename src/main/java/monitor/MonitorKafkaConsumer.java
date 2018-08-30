b05ac5bd8bc3ca6c8023277d9a65f20a54b565d2
lance4399 <lx_lance@163.com>
2018-08-30 18:10:11 +0800

    ss
    ss

--- src/main/java/monitor/MonitorKafkaConsumer.java
+++ src/main/java/monitor/MonitorKafkaConsumer.java
@@ -54,7 +54,7 @@ public class MonitorKafkaConsumer {
 
     public static void consumer(String indexNmae,String typeName,ElastisearchHelper esHelper) throws InterruptedException {
         Properties props = new Properties();
-        props.put("bootstrap.servers", "kfk013218.heracles.sohuno.com:9092,pdn096224.heracles.sohuno.com:9092,kfk013217.heracles.sohuno.com:9092,pdn096223.heracles.sohuno.com:9092,pdn096229.heracles.sohuno.com:9092,pdn096228.heracles.sohuno.com:9092,pdn096226.heracles.sohuno.com:9092,pdn096230.heracles.sohuno.com:9092,pdn096225.heracles.sohuno.com:9092,kfk013219.heracles.sohuno.com:9092,pdn096222.heracles.sohuno.com:9092,pdn096221.heracles.sohuno.com:9092,kfk013220.heracles.sohuno.com:9092" );
+        props.put("bootstrap.servers", "kfk" );
         props.put("group.id", "pv-test8");
         props.put("enable.auto.commit", "false");
         props.put("auto.commit.interval.ms", "1000");
